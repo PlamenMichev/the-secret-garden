5bc0e6f069d70e299066e7bb2acef590636ce932
Plamen Michev <plamenmichev15@gmail.com>
2023-11-20 09:41:55 +0100

    change net orientation depending on screen position

--- js/script.js
+++ js/script.js
@@ -31,23 +31,31 @@ const moveButterfly = function (x, y) {
   butterfly.style.top = y + "px";
 };
 
- function getRandomInt(max) {
-   return Math.floor(Math.random() * max);
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
 // //net movement
-$(document).on('mousemove', function(e){
-   $('#net').css({
-       left:  e.pageX,
-       top:   e.pageY
-    });
+$(document).on("mousemove", function (e) {
+  const isLeftPartOfScreen = e.pageX < window.innerWidth / 2;
+
+  if (!isLeftPartOfScreen) {
+    $("#net").css({ transform: "scaleX(-1)" });
+  } else {
+    $("#net").css({ transform: "scaleX(1)" });
+  }
+
+  $("#net").css({
+    left: e.pageX,
+    top: e.pageY,
+  });
 });
 
 //watering can rotation and waterdrops + flowers
 
 var rotation = 0;
-$('.waterdrop').hide();
-$('#flower').hide();
+$(".waterdrop").hide();
+$("#flower").hide();
 
 const wateringcan = document.querySelector("#wateringcan");
 let rec = wateringcan.getBoundingClientRect();
@@ -58,36 +66,39 @@ const height2 = rec.height;
 
 setInterval(function drop() {
   rand = getRandomInt(10);
-  $('.waterdrop').css({top: y2+height/10, left: x2-rand, position: 'absolute'})
-  $('.waterdrop').animate({top: screen.height, left: x2-rand});
-},750);
+  $(".waterdrop").css({
+    top: y2 + height / 10,
+    left: x2 - rand,
+    position: "absolute",
+  });
+  $(".waterdrop").animate({ top: screen.height, left: x2 - rand });
+}, 750);
 
-$('#flower').css({bottom: 0, left: x2 - 100, position: 'absolute'});
+$("#flower").css({ bottom: 0, left: x2 - 100, position: "absolute" });
 
-jQuery.fn.rotate = function(degrees) {
-    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
-                 '-moz-transform' : 'rotate('+ degrees +'deg)',
-                 '-ms-transform' : 'rotate('+ degrees +'deg)',
-                 'transform' : 'rotate('+ degrees +'deg)'});
-    return $(this);
+jQuery.fn.rotate = function (degrees) {
+  $(this).css({
+    "-webkit-transform": "rotate(" + degrees + "deg)",
+    "-moz-transform": "rotate(" + degrees + "deg)",
+    "-ms-transform": "rotate(" + degrees + "deg)",
+    transform: "rotate(" + degrees + "deg)",
+  });
+  return $(this);
 };
 
-$('#wateringcan').click(function() {
-    console.log(rotation);
-    if(rotation == 0) 
-    {
-      rotation = -25;
-      $('.waterdrop').show();
-      $('#flower').hide();
-    }
-    else if(rotation == -25)
-    {
-      rotation = 0;
-      $('.waterdrop').hide();
-      $('#flower').show();
-    }
-    $(this).rotate(rotation);
-    drop();
+$("#wateringcan").click(function () {
+  console.log(rotation);
+  if (rotation == 0) {
+    rotation = -25;
+    $(".waterdrop").show();
+    $("#flower").hide();
+  } else if (rotation == -25) {
+    rotation = 0;
+    $(".waterdrop").hide();
+    $("#flower").show();
+  }
+  $(this).rotate(rotation);
+  drop();
 });
 
 //apple positioning
@@ -103,28 +114,35 @@ let y = rect.y;
 let height = rect.height;
 let width = rect.width;
 
-apple1.style.left = (width/5 + getRandomInt(width/2) + x) + "px";
-apple2.style.left = (width/5 + getRandomInt(width/2) + x) + "px";
-apple3.style.left = (width/5 + getRandomInt(width/2) + x) + "px";
+apple1.style.left = width / 5 + getRandomInt(width / 2) + x + "px";
+apple2.style.left = width / 5 + getRandomInt(width / 2) + x + "px";
+apple3.style.left = width / 5 + getRandomInt(width / 2) + x + "px";
 
-apple1.style.top = (getRandomInt(height/2) + y + height/11) + "px";
-apple2.style.top = (getRandomInt(height/2) + y + height/11) + "px";
-apple3.style.top = (getRandomInt(height/2) + y + height/11) + "px";
+apple1.style.top = getRandomInt(height / 2) + y + height / 11 + "px";
+apple2.style.top = getRandomInt(height / 2) + y + height / 11 + "px";
+apple3.style.top = getRandomInt(height / 2) + y + height / 11 + "px";
 
 //woodpecker
-$('#woodpecker').hide();
+$("#woodpecker").hide();
 
 const wood = document.querySelector("#wood");
-wood.style.left = (x + width/2.4) + "px";
-wood.style.top = (y + height/1.5) + "px";
+wood.style.left = x + width / 2.4 + "px";
+wood.style.top = y + height / 1.5 + "px";
 
-$('#woodpecker').css({left: x + width/2.4, top: y + height/2, position: 'absolute'});
-
-$('#wood').hover(function(){
-  $('#woodpecker').show();
-}, function(){
-  $('#woodpecker').hide();
+$("#woodpecker").css({
+  left: x + width / 2.4,
+  top: y + height / 2,
+  position: "absolute",
 });
+
+$("#wood").hover(
+  function () {
+    $("#woodpecker").show();
+  },
+  function () {
+    $("#woodpecker").hide();
+  }
+);
 
 //apple movement
 const basket = document.querySelector(".basket");
@@ -135,41 +153,44 @@ let y1 = rect1.y;
 let height1 = rect1.height;
 let width1 = rect1.width;
 
-$('#apple1').click(function(){
-  $('#apple1').animate({top: y1 + height1/2, left: x1 + width1/(getRandomInt(10)+2)});
-})
+$("#apple1").click(function () {
+  $("#apple1").animate({
+    top: y1 + height1 / 2,
+    left: x1 + width1 / (getRandomInt(40) + 2),
+  });
+});
 
-$('#apple2').click(function(){
-  $('#apple2').animate({top: y1 + height1/2, left: x1 + width1/(getRandomInt(10)+2)});
-})
+$("#apple2").click(function () {
+  $("#apple2").animate({
+    top: y1 + height1 / 2,
+    left: x1 + width1 / (getRandomInt(40) + 2),
+  });
+});
 
-$('#apple3').click(function(){
-  $('#apple3').animate({top: y1 + height1/2, left: x1 + width1/(getRandomInt(10)+2)});
-})
+$("#apple3").click(function () {
+  $("#apple3").animate({
+    top: y1 + height1 / 2,
+    left: x1 + width1 / (getRandomInt(40) + 2),
+  });
+});
 
 //changing background color
-document.body.style.backgroundColor = 'aqua';
+document.body.style.backgroundColor = "aqua";
 
-document.addEventListener("keypress", function(event) {
+document.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
-    if(document.body.style.backgroundColor == 'aqua')
-    {
-        document.body.style.backgroundColor = '#00008B';
-        $("#apple1").attr('src',"images/apple_night.png");
-        $("#apple2").attr('src',"images/apple_night.png");
-        $("#apple3").attr('src',"images/apple_night.png");
-        $("#flower").attr('src',"images/flower_night.png");
-    }
-    else
-    {
-        document.body.style.backgroundColor = 'aqua';
-        $("#apple1").attr('src',"images/apple.png");
-        $("#apple2").attr('src',"images/apple.png");
-        $("#apple3").attr('src',"images/apple.png");
-        $("#flower").attr('src',"images/flower_day.png");
+    if (document.body.style.backgroundColor == "aqua") {
+      document.body.style.backgroundColor = "#00008B";
+      $("#apple1").attr("src", "images/apple_night.png");
+      $("#apple2").attr("src", "images/apple_night.png");
+      $("#apple3").attr("src", "images/apple_night.png");
+      $("#flower").attr("src", "images/flower_night.png");
+    } else {
+      document.body.style.backgroundColor = "aqua";
+      $("#apple1").attr("src", "images/apple.png");
+      $("#apple2").attr("src", "images/apple.png");
+      $("#apple3").attr("src", "images/apple.png");
+      $("#flower").attr("src", "images/flower_day.png");
     }
   }
 });
-
-
-
